1f3022d685dc4c9f326ce7fea36196141c620477
Venkata Saidurga Polamraju <saidurgacsea@gmail.com>
2024-08-29 18:25:35 +0530

    [ES-1580]
    Signed-off-by: Venkata Saidurga Polamraju <saidurgacsea@gmail.com>

--- mosip-identity-plugin/src/main/java/io/mosip/signup/plugin/mosipid/service/IdrepoProfileRegistryPluginImpl.java
+++ mosip-identity-plugin/src/main/java/io/mosip/signup/plugin/mosipid/service/IdrepoProfileRegistryPluginImpl.java
@@ -100,6 +100,9 @@ public class IdrepoProfileRegistryPluginImpl implements ProfileRegistryPlugin {
     @Value("#{'${mosip.signup.idrepo.optional-language:}'.split(',')}")
     private List<String> optionalLanguages;
 
+    @Value("${mosip.signup.idrepo.idvid-postfix}")
+    private String postfix;
+
     @Autowired
     @Qualifier("selfTokenRestTemplate")
     private RestTemplate restTemplate;
@@ -211,6 +214,7 @@ public class IdrepoProfileRegistryPluginImpl implements ProfileRegistryPlugin {
         try {
             IdRequestByIdDTO requestByIdDTO = new IdRequestByIdDTO();
             RequestWrapper<IdRequestByIdDTO> idDTORequestWrapper=new RequestWrapper<>();
+            individualId= StringUtils.isEmpty(postfix) ? individualId : individualId.concat(postfix);
             requestByIdDTO.setId(individualId);
             requestByIdDTO.setType("demo");
             requestByIdDTO.setIdType("HANDLE");
