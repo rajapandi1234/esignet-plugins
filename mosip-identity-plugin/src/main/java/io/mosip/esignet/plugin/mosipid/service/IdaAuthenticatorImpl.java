b3592d188a91f8a5b051dd1ecec600c9d66f54e8
pvsaidurga <saidurgacsea@gmail.com>
2025-08-18 15:33:12 +0530

    [ES-2479] Fixed nullpointer (#149)
    * [ES-2175] Fixed nullpointer
    
    Signed-off-by: pvsaidurga <saidurgacsea@gmail.com>
    
    * Fix to avoid NPE in IDA when unverifiedConsentedClaims is not present
    
    Signed-off-by: ase-101 <sunkadaeanusha@gmail.com>
    
    ---------
    
    Signed-off-by: pvsaidurga <saidurgacsea@gmail.com>
    Signed-off-by: ase-101 <sunkadaeanusha@gmail.com>
    Co-authored-by: ase-101 <sunkadaeanusha@gmail.com>

--- mosip-identity-plugin/src/main/java/io/mosip/esignet/plugin/mosipid/service/IdaAuthenticatorImpl.java
+++ mosip-identity-plugin/src/main/java/io/mosip/esignet/plugin/mosipid/service/IdaAuthenticatorImpl.java
@@ -111,6 +111,11 @@ public class IdaAuthenticatorImpl implements Authenticator {
     @Override
     public KycExchangeResult doKycExchange(String relyingPartyId, String clientId, KycExchangeDto kycExchangeDto)
             throws KycExchangeException {
+        return kycExchange(relyingPartyId,clientId,kycExchangeDto,false);
+    }
+
+    private KycExchangeResult kycExchange(String relyingPartyId, String clientId, KycExchangeDto kycExchangeDto,boolean isV2)
+            throws KycExchangeException {
         log.info("Started to build kyc-exchange request with transactionId : {} && clientId : {}",
                 kycExchangeDto.getTransactionId(), clientId);
         try {
@@ -129,7 +134,7 @@ public class IdaAuthenticatorImpl implements Authenticator {
             idaKycExchangeRequest.setRespType(kycExchangeDto.getUserInfoResponseType()); //may be either JWT or JWE
             idaKycExchangeRequest.setIndividualId(kycExchangeDto.getIndividualId());
 
-            if(kycExchangeDto instanceof VerifiedKycExchangeDto){
+            if(isV2){
                 setClaims((VerifiedKycExchangeDto) kycExchangeDto, idaKycExchangeRequest);
             }
 
@@ -138,7 +143,7 @@ public class IdaAuthenticatorImpl implements Authenticator {
             //set signature header, body and invoke kyc exchange endpoint
             String requestBody = objectMapper.writeValueAsString(idaKycExchangeRequest);
             RequestEntity requestEntity = RequestEntity
-                    .post(UriComponentsBuilder.fromUriString((kycExchangeDto instanceof VerifiedKycExchangeDto) ?
+                    .post(UriComponentsBuilder.fromUriString((isV2) ?
                             kycExchangeUrlV2 : kycExchangeUrl).pathSegment(relyingPartyId,
                             clientId).build().toUri())
                     .contentType(MediaType.APPLICATION_JSON_UTF8)
@@ -179,11 +184,7 @@ public class IdaAuthenticatorImpl implements Authenticator {
                 idaKycExchangeRequest.setVerifiedConsentedClaims(verifiedClaimsList);
             }
 
-            Map<String, JsonNode> unVerifiedConsentedClaims = getUnVerifiedConsentedClaims(acceptedClaimDetails);
-            if(!CollectionUtils.isEmpty(unVerifiedConsentedClaims)){
-                Map<String, Object> unVerifiedConsentedClaim = objectMapper.convertValue(unVerifiedConsentedClaims, new TypeReference<>() {});
-                idaKycExchangeRequest.setUnVerifiedConsentedClaims(unVerifiedConsentedClaim);
-            }
+            idaKycExchangeRequest.setUnVerifiedConsentedClaims(getUnVerifiedConsentedClaims(acceptedClaimDetails));
         }
     }
 
@@ -367,7 +368,7 @@ public class IdaAuthenticatorImpl implements Authenticator {
 
     @Override
     public KycExchangeResult doVerifiedKycExchange(String relyingPartyId, String clientId, VerifiedKycExchangeDto kycExchangeDto) throws KycExchangeException {
-        return doKycExchange(relyingPartyId, clientId, kycExchangeDto);
+        return kycExchange(relyingPartyId, clientId, kycExchangeDto,true);
     }
 
     /**
@@ -376,7 +377,7 @@ public class IdaAuthenticatorImpl implements Authenticator {
      * @return un verified consented claims
      */
     @NotNull // This is added to not return null either return un verified claims map or empty map
-    private Map<String, JsonNode> getUnVerifiedConsentedClaims(Map<String, JsonNode> acceptedClaimDetails) {
+    private Map<String, Object> getUnVerifiedConsentedClaims(Map<String, JsonNode> acceptedClaimDetails) {
         Map<String, JsonNode> unVerifiedConsentedClaims = new HashMap<>();
         if(!CollectionUtils.isEmpty(acceptedClaimDetails)) {
             for(Map.Entry<String, JsonNode> entry : acceptedClaimDetails.entrySet()) {
@@ -387,6 +388,6 @@ public class IdaAuthenticatorImpl implements Authenticator {
                 }
             }
         }
-        return unVerifiedConsentedClaims;
+        return objectMapper.convertValue(unVerifiedConsentedClaims, new TypeReference<>() {});
     }
 }
